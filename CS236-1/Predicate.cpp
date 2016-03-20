6f3107c743ba9ddf5b416755620fa62f8ce956c4
Shaun <shaunmiller236@gmail.com>
2016-03-19 19:03:33 -0600

    Added Predicate class and header

---
+++ CS236-1/Predicate.cpp
@@ -0,0 +1,9 @@
+//
+//  Predicate.cpp
+//  CS236-1
+//
+//  Created by Shaun Miller on 3/19/16.
+//  Copyright © 2016 Shaun Miller. All rights reserved.
+//
+
+#include "Predicate.hpp"
---
+++ CS236-1/Predicate.hpp
@@ -0,0 +1,14 @@
+//
+//  Predicate.hpp
+//  CS236-1
+//
+//  Created by Shaun Miller on 3/19/16.
+//  Copyright © 2016 Shaun Miller. All rights reserved.
+//
+
+#ifndef Predicate_hpp
+#define Predicate_hpp
+
+#include <stdio.h>
+
+#endif /* Predicate_hpp */
